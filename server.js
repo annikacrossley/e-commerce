d975d245ac244bb5aa7298264453ba35b25d0d8d
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-10-11 21:33:48 -0500

    models moved to separate files and connected

--- server.js
+++ server.js
@@ -2,90 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 
 // import sequelize connection
-const {Sequelize, DataTypes, Model} = require('sequelize')
-const sequelize = new Sequelize('thirteen_db', 'root', 'Teague50!', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
+const sequelize = require('./config/connection')
 
-//Models
-//Category
-class Category extends Model {}
-Category.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  category_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-})
-
-//Product
-class Product extends Model {}
-Product.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  product_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-    validate: true
-  },
-  stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 10,
-    validate: true
-  },
-  category_id: {
-    type: DataTypes.INTEGER
-    //references the category model's ID 
-  }
-})
-
-//Tag
-class Tag extends Model {}
-Tag.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  tag_name: {
-    type: DataTypes.STRING
-  }
-})
-
-//Product Tag
-class ProductTag extends Model {}
-ProductTag.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  product_id: {
-    type: DataTypes.INTEGER
-    //references the product model's ID
-  },
-  tag_id: {
-    type: DataTypes.INTEGER
-    //references the tag model's ID
-  }
-})
+require('./models')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
